3b2263221219eb57782c4053a1baa43465d09014
Diego Coelho <diegocoelho.13@gmail.com>
2016-11-08 16:06:10 -0300

    Configura body-parser e request

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 'use strict';
 
 const
-  express = require('express');
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  request = require('request');
 
 var app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
+app.use(express.static('public'));
+
 var config = require('./server/config/config');
 require('./server/config/routes')(app,config);
 
