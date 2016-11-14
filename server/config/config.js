9c78b15f47f09b6f0375ad0e119fa292689d1573
Diego Coelho <diegocoelho.13@gmail.com>
2016-11-14 16:36:13 -0300

    Processa mensagens de texto consultando reservatórios similares

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.PORT || 5002,
-  api: 'https://wwws-cloud.lsd.ufcg.edu.br:42160/api/',
+  api: 'http://web.cloud.lsd.ufcg.edu.br:42160/api/',
   fb : {
     validation_token : 'meudeuduceuberg'
   }
