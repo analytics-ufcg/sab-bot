ddcd6bcbb8e7276eaeb03aa870d18b3d66180234
Jefferson Neves <jefferson.rpn@gmail.com>
2016-12-09 12:11:36 -0300

    Bolsa not found

--- img.js
+++ img.js
@@ -14,75 +14,108 @@ http.createServer(function (req, res) {
               y = 130,
               line = 30,
               centerX = width/2,
-              centerY = (height/2) + (y/2),
-              circleRadius = 80;
+              centerY = (height/2) + 80,
+              circleRadius = 80,
+              circleCenterX = centerX + 60,
+              circleCenterY = centerY,
+              lineWidth = 120,
+              percent = 0.1,
+              unit = ' hm³',
+              max = 1234 + unit,
+              volume = 25 + unit;
 
           var canvas = new Canvas(width, height)
           var ctx = canvas.getContext('2d')
 
           ctx.fillStyle = '#6F7575'
           ctx.fillRect(0, 0, width, height)
-          ctx.save()
 
           ctx.fillStyle = '#FFFFFF'
           ctx.fillRect(0, 0, width, 100)
-          ctx.save()
 
           ctx.strokeStyle = '#FFFFFF'
           ctx.strokeRect(padding, padding, 380, 430)
-          ctx.save()
 
           ctx.strokeStyle = '#6F7575'
           ctx.strokeRect(padding, padding, 380, 430)
-          ctx.save()
 
           ctx.fillStyle = '#FFFFFF'
-          ctx.font = '22px Open Sans'
+          ctx.font = '22px Arial'
           ctx.fillText('Açude Epitácio Pessoa', x, y)
-          ctx.font = '20px Open Sans'
+          ctx.font = '20px Arial'
           ctx.fillText('Boqueirão', x, (y += line))
-          ctx.font = '18px Open Sans'
-          ctx.fillText('Última medição: 01/12/2016', x, (y += line))
+          ctx.font = '16px Arial'
+          ctx.fillText('Última medição em 01/12/2016', x, (height - line))
+
+          var img = new Canvas.Image
+          img.src = data
+          ctx.drawImage(img, centerX - (img.width/2), padding+5, img.width, img.height)
           ctx.save()
 
-          var img = new Canvas.Image; // Create a new Image
-          img.src = data;
-          ctx.drawImage(img, centerX - (img.width/2), padding+5, img.width, img.height);
+          ctx.font = '16px Arial'
+          var width = ctx.measureText(text).width
+          ctx.fillText(max, circleCenterX - lineWidth - width, centerY-circleRadius-6)
 
-          ctx.fillStyle = "rgba(0, 0, 200, 0)";;
-          ctx.strokeStyle = '#FF0000'
-          ctx.lineWidth = 3
           ctx.beginPath()
-          ctx.arc(centerX, centerY, circleRadius, 0, Math.PI * 2, true)
+          ctx.strokeStyle = '#305479'
+          ctx.lineWidth = 6
+          ctx.moveTo(circleCenterX, centerY-circleRadius+3)
+          ctx.lineTo(circleCenterX - lineWidth, centerY-circleRadius+3)
+          ctx.stroke()
+          ctx.closePath()
+
+          ctx.font = '16px Arial'
+          var width = ctx.measureText(text).width
+          ctx.fillText(volume, circleCenterX - lineWidth - width, centerY + circleRadius - (percent*2*circleRadius)+22)
+
+          ctx.beginPath()
+          ctx.moveTo(circleCenterX, centerY + circleRadius - (percent*2*circleRadius)+3)
+          ctx.lineTo(circleCenterX - lineWidth, centerY + circleRadius - (percent*2*circleRadius)+3)
+          ctx.stroke()
+          ctx.closePath()
+
+          ctx.restore()
+          ctx.fillStyle = '#FFFFFF'
+          ctx.beginPath()
+          ctx.arc(circleCenterX, centerY, circleRadius, 0, Math.PI * 2, true)
           ctx.fill()
           ctx.stroke()
           ctx.closePath()
-          ctx.save()
           ctx.clip()
 
-          var onda = new Canvas.Image;
-          var percent = 0.35;
-          onda.src = data_onda;
-          ctx.drawImage(onda, centerX - circleRadius, centerY + circleRadius - (percent*2*circleRadius) - 8,2*circleRadius, 2*circleRadius + 10);
+          var onda = new Canvas.Image
+          onda.src = data_onda
+          ctx.drawImage(onda, circleCenterX - circleRadius, centerY + circleRadius - (percent*2*circleRadius) - 8, 2*circleRadius, 2*circleRadius + 20)
 
+          ctx.fillStyle = '#305479'
+          ctx.font = '36px Arial'
+          var text = (percent*100)+'%'
+          var width = ctx.measureText(text).width
+          ctx.fillText(text, circleCenterX - (width/2), centerY + 15)
 
+          // ctx.fillStyle = '#FFFFFF'
+          // ctx.font = '42px Arial'
+          // var text = '100%'
+          // var width = ctx.measureText(text).width
+          // ctx.fillText(text, centerX - (width/2), centerY + 15)
+          // ctx.save()
 
-          ctx.fillStyle = '#6F7575'
-          ctx.font = '42px Open Sans'
-          var text = '100%'
-          var width = ctx.measureText(text).width;
-          ctx.fillText(text, centerX - (width/2), centerY + 15)
-          ctx.save()
+          ctx.fillStyle = 'rgba(0,0,0,0 )'
+          ctx.strokeStyle = '#305479'
+          ctx.lineWidth = 6
+          ctx.beginPath()
+          ctx.arc(circleCenterX, centerY, circleRadius-3, 0, Math.PI * 2, true)
+          ctx.fill()
+          ctx.stroke()
+          ctx.closePath()
 
-
-
-          res.write('<html><body>');
-          res.write('<img src="' + canvas.toDataURL() + '" />');
-          res.write('</body></html>');
+          res.write('<html><body>')
+          res.write('<img src="' + canvas.toDataURL() + '" />')
+          res.write('</body></html>')
           res.end();
         });
     });
 
-}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
+}).listen(8124, '127.0.0.1')
+console.log('Server running at http://127.0.0.1:8124/')
 var Canvas = require('canvas')
